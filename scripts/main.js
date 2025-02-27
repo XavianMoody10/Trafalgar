59c55b8a706fdf4f212e2ae44c57e4c8f7e40d86
Xavian Moody <XavianMoody1025@gmail.com>
2025-02-26 21:19:23 -0500

    Finished apps section

--- scripts/main.js
+++ scripts/main.js
@@ -27,4 +27,30 @@ function mobileNavigation() {
   });
 }
 
+// Execute animation when scrolling to apps section
+function appSectionAnimation() {
+  const appFlexElms = document.querySelectorAll(".app-flex");
+
+  function callback(entries) {
+    entries.forEach((entry) => {
+      if (entry.isIntersecting) {
+        if (entry.target.classList.contains("app-flex--reverse")) {
+          entry.target.children[1].classList.add("animation-enter-right");
+          entry.target.children[0].classList.add("animation-enter-left");
+        } else {
+          entry.target.children[0].classList.add("animation-enter-left");
+          entry.target.children[1].classList.add("animation-enter-right");
+        }
+      }
+    });
+  }
+
+  let observer = new IntersectionObserver(callback, { threshold: 0.5 });
+
+  appFlexElms.forEach((a) => {
+    observer.observe(a);
+  });
+}
+
 mobileNavigation();
+appSectionAnimation();
