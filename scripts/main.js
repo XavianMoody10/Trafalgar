baae83ef8c5c9ea0f30039a4772d331043702b2a
Xavian Moody <XavianMoody1025@gmail.com>
2025-02-28 00:15:50 -0500

    Finished testimonial section

--- scripts/main.js
+++ scripts/main.js
@@ -13,11 +13,12 @@ function mobileNavigation() {
 
   function toggleNavigation() {
     menuIsOpen = !menuIsOpen;
+    const navigationTransform = navigation.style.transform;
 
     if (menuIsOpen) {
-      navigation.style.transform = "translateX(0)";
+      navigationTransform = "translateX(0)";
     } else {
-      navigation.style.transform = "translateX(-100%)";
+      navigationTransform = "translateX(-100%)";
     }
   }
 
@@ -33,13 +34,15 @@ function appSectionAnimation() {
 
   function callback(entries) {
     entries.forEach((entry) => {
+      const children = entry.target.children;
+
       if (entry.isIntersecting) {
         if (entry.target.classList.contains("app-flex--reverse")) {
-          entry.target.children[1].classList.add("animation-enter-right");
-          entry.target.children[0].classList.add("animation-enter-left");
+          children[1].classList.add("animation-enter-right");
+          children[0].classList.add("animation-enter-left");
         } else {
-          entry.target.children[1].classList.add("animation-enter-left");
-          entry.target.children[0].classList.add("animation-enter-right");
+          children[1].classList.add("animation-enter-left");
+          children[0].classList.add("animation-enter-right");
         }
       }
     });
@@ -52,5 +55,45 @@ function appSectionAnimation() {
   });
 }
 
+function testimonialSlider() {
+  let currentSlide = 0;
+  const testimonies = document.querySelectorAll(".testimony");
+  const testimonyTrack = document.querySelector(".testimonial-slider__track");
+  const arrows = document.querySelectorAll(".testimonial-controls__arrow");
+  const bullets = document.querySelectorAll(".testimonial-controls__bullet");
+
+  // Add and remove active class from bullets
+  function toggleBulletClass(index) {
+    const activeClass = "testimonial-controls__bullet--active";
+
+    bullets.forEach((b, i) => {
+      if (index === i) {
+        b.classList.add(activeClass);
+      } else {
+        b.classList.remove(activeClass);
+      }
+    });
+  }
+
+  // EVENTLISTER FOR PREVIOUS ARROW BUTTON
+  arrows[0].addEventListener("click", () => {
+    if (currentSlide > 0) {
+      currentSlide = currentSlide - 1;
+      toggleBulletClass(currentSlide);
+      testimonyTrack.style.transform = `translateX(-${100 * currentSlide}%)`;
+    }
+  });
+
+  // EVENTLISTER FOR NEXT ARROW BUTTON
+  arrows[1].addEventListener("click", () => {
+    if (currentSlide < testimonies.length - 1) {
+      currentSlide = currentSlide + 1;
+      toggleBulletClass(currentSlide);
+      testimonyTrack.style.transform = `translateX(-${100 * currentSlide}%)`;
+    }
+  });
+}
+
 mobileNavigation();
 appSectionAnimation();
+testimonialSlider();
